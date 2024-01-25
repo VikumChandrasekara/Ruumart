22ec2bf65323e60137185781fb417734fa5e0957
Vikum Chandrasekara <chandrasekara.vikum@gmail.com>
2024-01-25 08:50:58 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './Assests/Images/Ruumart Group Logo white.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Thank you for visiting Ruumart.com<br></br>The website will be released soon
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
